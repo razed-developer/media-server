1614387ce3e567283323a7d06f212f87d36132b9
razed-developer <razed-developer@outlook.com>
2026-08-18 23:27:01 -0700

    Enable remote navigation in desktop and browser clients

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installRemoteNavigation } from './remoteNavigation';
 import './styles.css';
 import './media.css';
+import './remote.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+installRemoteNavigation();

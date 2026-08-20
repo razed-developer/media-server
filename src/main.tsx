3f43e27e03b9df8f86c2e808a2d6e9a8ab7a1978
razed-developer <razed-developer@outlook.com>
2026-08-20 16:46:59 -0700

    Mount startup warmup and episode social helpers

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { FullscreenGuard } from './components/FullscreenGuard';
+import { LibraryJumpBridge } from './components/LibraryJumpBridge';
 import { OnyxDialogProvider } from './components/OnyxDialogProvider';
+import { PlayerEpisodeSocial } from './components/PlayerEpisodeSocial';
 import { PlayerSubtitleSearch } from './components/PlayerSubtitleSearch';
 import { SetupGate } from './components/SetupGate';
+import { StartupWarmup } from './components/StartupWarmup';
 import { installRemoteNavigation } from './remoteNavigation';
 import { installScrollEnhancements } from './scrollEnhancements';
 import './styles.css';
 import './media.css';
 import './onyx-features.css';
+import './userFeatures.css';
 import './libraryCompact.css';
 import './liveChannels.css';
 import './fullscreen.css';
@@ -19,13 +23,17 @@ import './libraryRoots.css';
 import './remote.css';
 import './subtitleFinder.css';
 import './playbackContinuity.css';
+import './startupWarmup.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <OnyxDialogProvider>
       <SetupGate><App /></SetupGate>
+      <StartupWarmup />
+      <LibraryJumpBridge />
       <FullscreenGuard />
       <PlayerSubtitleSearch />
+      <PlayerEpisodeSocial />
     </OnyxDialogProvider>
   </React.StrictMode>,
 );

ea13eb14d7811635d6a6cc281daf59bad79b0a46
razed-developer <razed-developer@outlook.com>
2026-08-20 07:30:06 -0700

    Load setup settings and music styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { SetupGate } from './components/SetupGate';
 import { installRemoteNavigation } from './remoteNavigation';
 import './styles.css';
 import './media.css';
+import './onyx-features.css';
 import './remote.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

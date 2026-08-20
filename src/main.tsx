0c97e3170b36f72d6ec74994692a8244a62cf42e
razed-developer <razed-developer@outlook.com>
2026-08-20 15:01:30 -0700

    Mount player subtitle search integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { FullscreenGuard } from './components/FullscreenGuard';
 import { OnyxDialogProvider } from './components/OnyxDialogProvider';
+import { PlayerSubtitleSearch } from './components/PlayerSubtitleSearch';
 import { SetupGate } from './components/SetupGate';
 import { installRemoteNavigation } from './remoteNavigation';
 import { installScrollEnhancements } from './scrollEnhancements';
@@ -16,12 +17,14 @@ import './scrollEnhancements.css';
 import './onyxDialogs.css';
 import './libraryRoots.css';
 import './remote.css';
+import './subtitleFinder.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <OnyxDialogProvider>
       <SetupGate><App /></SetupGate>
       <FullscreenGuard />
+      <PlayerSubtitleSearch />
     </OnyxDialogProvider>
   </React.StrictMode>,
 );

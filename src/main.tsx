dd92b321408d0af2dd8ff289c609398af7117164
razed-developer <razed-developer@outlook.com>
2026-08-20 10:50:04 -0700

    Wire fullscreen exit helper into Onyx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { FullscreenGuard } from './components/FullscreenGuard';
 import { SetupGate } from './components/SetupGate';
 import { installRemoteNavigation } from './remoteNavigation';
 import './styles.css';
@@ -8,11 +9,13 @@ import './media.css';
 import './onyx-features.css';
 import './libraryCompact.css';
 import './liveChannels.css';
+import './fullscreen.css';
 import './remote.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SetupGate><App /></SetupGate>
+    <FullscreenGuard />
   </React.StrictMode>,
 );
 
